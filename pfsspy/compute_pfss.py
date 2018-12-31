77c62f524e857e68e85df8f1f611767c563332ec
David Stansby <dstansby@gmail.com>
2018-12-31 11:46:31 +0000

    Code tidy

--- pfsspy/compute_pfss.py
+++ pfsspy/compute_pfss.py
@@ -3,7 +3,7 @@ Script for reading in map of Br(theta, phi) on the solar surface, computing a
 PFSS extrapolation, and outputting to a netcdf file.
 """
 import sys
-import numpy as n
+import numpy as np
 from scipy.interpolate import interp2d
 import matplotlib.pyplot as plt
 from pfss import pfss
@@ -11,45 +11,52 @@ from pfss import pfss
 # DEFINE GRID FOR PFSS COMPUTATION
 # - equally spaced in rho=log(r/Rsun), s=cos(theta) and phi.
 # - specify number of grid points:
-nr = 60
-ns = 180
-np = 360
-if ((np%2)!=0):
-    sys.exit('ERROR: np must be even (for polar boundary conditions)')
+nr = 60     # Radial
+ns = 180    # Cos(theta)
+nphi = 360    # Phi
+if ((nphi % 2) != 0):
+    raise RuntimeError(
+        'Number of points in phi must be even (for polar boundary conditions)')
 # - choose source surface radius r (in Rsun):
 rss = 2.5
 
 # INPUT MAP OF br(theta,phi)
-d = 180.0/n.pi
+d = 180.0 / np.pi
 # - read in map from SFT simulation:
-br0 = n.loadtxt('test.dat')    # DEMO -- replace this with your own map
-ns0 = n.size(br0, axis=0)
-np0 = n.size(br0, axis=1)
+br0 = np.loadtxt('test.dat')    # DEMO -- replace this with your own map
+ns0 = np.size(br0, axis=0)
+np0 = np.size(br0, axis=1)
+
 # - original grid:
-th0 = n.linspace(0, n.pi, ns0)
-ph0 = n.linspace(0, 2*n.pi, np0)
+th0 = np.linspace(0, np.pi, ns0)
+ph0 = np.linspace(0, 2 * np.pi, np0)
+
 # - pfss grid:
-dp = 2*n.pi/np
-ds = 2/ns
-pc = n.linspace(0.5*dp, 2*n.pi - 0.5*dp, np)
-sc = n.linspace(-1 + 0.5*ds, 1 - 0.5*ds, ns)
-thc = n.flip(n.arccos(sc), 0)
+dphi = 2 * np.pi / nphi
+ds = 2 / ns
+
+pc = np.linspace(0.5 * dp, 2 * np.pi - 0.5 * dp, np)
+sc = np.linspace(-1 + 0.5 * ds, 1 - 0.5 * ds, ns)
+thc = np.flip(np.arccos(sc), 0)
+
 # - rotate to Carrington frame:
-clon = 290.7/d
-pc1 = (pc - clon + 2*n.pi) % (2*n.pi)
+clon = 290.7 / d
+pc1 = (pc - clon + 2 * np.pi) % (2 * np.pi)
 # - interpolate input map to pfss grid:
-bri = interp2d(ph0, th0, br0, kind='cubic', copy=True, bounds_error=False, fill_value=0)
-br = n.zeros((ns,np))
+bri = interp2d(ph0, th0, br0, kind='cubic', copy=True,
+               bounds_error=False, fill_value=0)
+br = np.zeros((ns, nphi))
 for i in range(np):
-    br[:,i] = bri(pc1[i], thc).flatten()
+    br[:, i] = bri(pc1[i], thc).flatten()
+
 # - correct flux balance (otherwise PFSS solver won't work):
-ipos = n.where(br > 0)
-ineg = n.where(br < 0)
-fluxp = n.abs(n.sum(br[ipos]))
-fluxn = n.abs(n.sum(br[ineg]))
-fluxmn = 0.5*(fluxp + fluxn)
-br[ineg] = br[ineg]*fluxmn/fluxn
-br[ipos] = br[ipos]*fluxmn/fluxp
+ipos = np.where(br > 0)
+ineg = np.where(br < 0)
+fluxp = np.abs(np.sum(br[ipos]))
+fluxn = np.abs(np.sum(br[ineg]))
+fluxmn = 0.5 * (fluxp + fluxn)
+br[ineg] = br[ineg] * fluxmn / fluxn
+br[ipos] = br[ipos] * fluxmn / fluxp
 
 
 # PLOT INPUT MAP BEFORE AND AFTER:
@@ -61,21 +68,21 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
 ax = plt.subplot(211)
-lat0 = 0.5*n.pi - th0
-pm = ax.pcolormesh(ph0*d, lat0*d, br0, cmap='bwr')
+lat0 = 0.5 * np.pi - th0
+pm = ax.pcolormesh(ph0*d, lat0 * d, br0, cmap='bwr')
 pm.set_clim(vmin=-bmax, vmax=bmax)
 cb1 = plt.colorbar(pm)
-#cb1.set_clim(vmin=-bmax, vmax=bmax)
+# cb1.set_clim(vmin=-bmax, vmax=bmax)
 ax.set_xlabel('Carrington longitude')
 ax.set_ylabel('Latitude')
 ax.set_title('Original input data')
 
 ax = plt.subplot(212)
-lat = 0.5*n.pi - thc
-pm = ax.pcolormesh(pc*d, lat*d, br, cmap='bwr')
+lat = 0.5 * np.pi - thc
+pm = ax.pcolormesh(pc * d, lat * d, br, cmap='bwr')
 pm.set_clim(vmin=-bmax, vmax=bmax)
 cb1 = plt.colorbar(pm)
-#cb1.set_clim(vmin=-bmax, vmax=bmax)
+# cb1.set_clim(vmin=-bmax, vmax=bmax)
 ax.set_xlabel('Carrington longitude')
 ax.set_ylabel('Latitude')
 ax.set_title('Mapped to PFSS grid')
@@ -87,5 +94,5 @@ plt.show()
 # COMPUTE POTENTIAL FIELD:
 # (note that output='bg' gives the output B averaged to grid points)
 print('Computing PFSS...')
-br = br[::-1,:]
+br = br[::-1, :]
 pfss(br, nr, ns, np, rss, filename='./bPF.nc', output='bg', testQ=False)
