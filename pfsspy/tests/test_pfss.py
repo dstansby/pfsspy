f61b476e2b19c90a516649155a5e107b0af5cfce
David Stansby <dstansby@gmail.com>
2018-12-31 23:08:13 +0000

    Make output optional

--- pfsspy/tests/test_pfss.py
+++ pfsspy/tests/test_pfss.py
@@ -5,7 +5,7 @@ import pfsspy
 def test_zeros():
     # Test a completely zero input
     ns = 30
-    nphi = 30
+    nphi = 20
     nr = 10
     rss = 2.5
     br = np.zeros((ns, nphi))
@@ -14,3 +14,7 @@ def test_zeros():
     out = pfsspy.pfss(br, nr, ns, nphi, rss, output='none')
     for comp in (out.alr, out.als, out.alp):
         assert np.all(comp == 0)
+
+    assert out.alr.shape == (nphi + 1, ns + 1, nr)
+    assert out.als.shape == (nphi + 1, ns, nr + 1)
+    assert out.alp.shape == (nphi, ns + 1, nr + 1)
