5029d8a68fc3a6bcb4af568746ac2fd9da38ddba
David Stansby <dstansby@gmail.com>
2019-01-25 22:30:46 +0000

    Fix install requires

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='pfsspy',
                    'Programming Language :: Python :: 3',
                    'Topic :: Scientific/Engineering :: Physics'],
       url='https://github.com/dstansby/pfsspy',
-      install_requires=['numpy', 'scipy', 'matplotlib'],
+      install_requires=['numpy', 'scipy', 'matplotlib', 'sunpy', 'astropy'],
       python_requires='>=3.5',
       packages=['pfsspy'],
       )
