a209861d6e80c355588e90c808c5ee6bc2228fff
David Stansby <dstansby@gmail.com>
2019-02-22 17:52:04 +0000

    Add warning to top of file

--- pfsspy/interpolator.py
+++ pfsspy/interpolator.py
@@ -2,7 +2,10 @@
 Interpolator functions
 
 This module contains a version of scipy.interpolate.RegularGridInterpolator,
-which has been edited for performance
+which has been edited for performance.
+
+THE CODE HERE IS LIABLE TO CHANGE/BREAK AT ANY TIME. Do not use this code
+outside of pfsspy.
 """
 import itertools
 import numpy as np
