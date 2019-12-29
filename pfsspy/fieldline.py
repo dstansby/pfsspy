Fix tests and exapansion factor calculation

pfsspy/fieldline.py
@@ -250,8 +250,8 @@ class FieldLine:
             return interpolator(phi, s)
 
         # Get output magnetic field, and calculate |B|
-        br, bs, bphi = self._output.bg
-        modb = np.sqrt(br**2 + bs**2 + bphi**2)
+        bg = self._output.bg
+        modb = np.linalg.norm(bg, axis=-1)
         # Interpolate at each end of field line
         b_solar = interp(modb[:, :, 0], solar_foot)[0, 0]
         b_source = interp(modb[:, :, -1], source_foot)[0, 0]

pfsspy/tests/test_pfss.py
@@ -151,13 +151,9 @@ def test_shape(zero_map):
     assert bs.shape == (nphi + 2, ns + 1, nr + 2)
     assert bp.shape == (nphi + 1, ns + 2, nr + 2)
 
-    br, bs, bp = out.bg
-    for comp in (br, bs, bp):
-        assert np.all(comp == 0)
-
-    assert br.shape == (nphi + 1, ns + 1, nr + 1)
-    assert bs.shape == (nphi + 1, ns + 1, nr + 1)
-    assert bp.shape == (nphi + 1, ns + 1, nr + 1)
+    bg = out.bg
+    assert np.all(bg == 0)
+    assert bg.shape == (nphi + 1, ns + 1, nr + 1, 3)
 
 
 def test_sunpy_map_input(zero_map):