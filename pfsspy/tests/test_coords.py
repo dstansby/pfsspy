5f36c7fb4833fc4adc9c7716e6a0edb3489f585f
David Stansby <dstansby@gmail.com>
2020-01-01 20:42:58 +0000

    Remove extra print statements in tests

--- pfsspy/tests/test_coords.py
+++ pfsspy/tests/test_coords.py
@@ -5,9 +5,6 @@ import numpy as np
 def test_forward_backwards():
     x, y, z = 1, 0, 0
     rho, s, phi = pfsspy.coords.cart2strum(x, y, z)
-    print(rho)
-    print(s)
-    print(phi)
     x1, y1, z1 = pfsspy.coords.strum2cart(rho, s, phi)
 
     assert np.allclose(np.array([x, y, z]), np.array([x1, y1, z1]))
