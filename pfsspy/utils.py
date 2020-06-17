7b1770c9e47e2df8a55da47c5f64b856489018f3
David Stansby <dstansby@gmail.com>
2020-06-17 11:40:05 +0100

    Clean up utils.py

--- pfsspy/utils.py
+++ pfsspy/utils.py
@@ -1,10 +1,14 @@
-import sunpy.map,sunpy.io
+import sunpy.map
+import sunpy.io
 import numpy as np
-import os, warnings,copy
+import os
+import warnings
+import copy
 from scipy import interpolate
 from astropy import units as u
 
-def load_adapt(adapt_path) :
+
+def load_adapt(adapt_path):
     """ Parse adapt .fts file as a `sunpy.map.MapSequence`
 
     Description
@@ -12,18 +16,18 @@ def load_adapt(adapt_path) :
     ADAPT magnetograms contain 12 realizations and their data
     attribute consists of a 3D data cube where each slice is
     the data corresponding to a separate realization of the
-    magnetogram. This function loads the raw fits file and 
+    magnetogram. This function loads the raw fits file and
     parses it to a `sunpy.map.MapSequence` object containing
     a `sunpy.map.Map` instance for each realization.
-    
+
     Parameters
     ----------
-    adapt_path : `str` 
+    adapt_path : `str`
         filepath corresponding to an ADAPT .fts file
-    
+
     Returns
     -------
-    adaptMapSequence : `sunpy.map.MapSequence` 
+    adaptMapSequence : `sunpy.map.MapSequence`
         a `sunpy.map.MapSequence` instance containing a
         `sunpy.map.Map` instance for each ADAPT realization.
 
@@ -31,12 +35,7 @@ def load_adapt(adapt_path) :
     adapt_fits = sunpy.io.fits.read(adapt_path)
     assert adapt_fits[0].header.get('MODEL') == 'ADAPT', \
         f"{os.path.basename(adapt_path)} header['MODEL'] is not 'ADAPT' "
-    data_header_pairs = [
-                        (map_slice,adapt_fits[0].header) 
-                         for map_slice in adapt_fits[0].data
-                        ]
-    adaptMapSequence = sunpy.map.Map(data_header_pairs,sequence=True) 
+    data_header_pairs = [(map_slice, adapt_fits[0].header)
+                         for map_slice in adapt_fits[0].data]
+    adaptMapSequence = sunpy.map.Map(data_header_pairs, sequence=True)
     return adaptMapSequence
-
-
-
