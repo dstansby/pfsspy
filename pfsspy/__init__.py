7aeb925f71b6f83d26a7cf09fdf0ab0378ac0e0f
STBadman <samuel_badman@berkeley.edu>
2020-09-16 18:46:48 -0700

    Fix build issue with escape characters

--- pfsspy/__init__.py
+++ pfsspy/__init__.py
@@ -570,9 +570,9 @@ class Output:
         The conversion which relates the spherical and cartesian 
         coordinates is as follows:
 
-        .. math:: B_R = sin\\theta cos\phi B_x + sin\\theta sin\phi B_y + cos\\theta B_z 
-        .. math:: B_\\theta = cos\\theta cos\phi B_x + cos\\theta sin\phi B_y - sin\\theta B_z 
-        .. math:: B_\phi = -sin\phi B_x + cos\phi B_y 
+        .. math:: B_R = sin\\theta cos\\phi B_x + sin\\theta sin\\phi B_y + cos\\theta B_z 
+        .. math:: B_\\theta = cos\\theta cos\\phi B_x + cos\\theta sin\\phi B_y - sin\\theta B_z 
+        .. math:: B_\\phi = -sin\\phi B_x + cos\\phi B_y 
 
         The above equations may be written as a (3x3) matrix and 
         inverted to retrieve the inverse transformation (cartesian from spherical)
