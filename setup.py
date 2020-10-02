c1769d3c853620c1a52bb7da90a77035a8dc99d5
David Stansby <dstansby@gmail.com>
2020-10-02 14:07:50 +0100

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -5,22 +5,5 @@ import versioneer
 if sys.version_info < (3, 5):
     sys.exit('Python versions older than 3.5 are not supported.')
 
-setup(name='pfsspy',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      description='Potential Field Source Surface model package',
-      author='David Stansby',
-      license='GPL3',
-      author_email='dstansby@gmail.com',
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Console',
-                   'Intended Audience :: Science/Research',
-                   'Natural Language :: English',
-                   'Programming Language :: Python :: 3',
-                   'Topic :: Scientific/Engineering :: Physics'],
-      url='https://github.com/dstansby/pfsspy',
-      install_requires=['numpy', 'scipy', 'sunpy!=2.0.2', 'astropy>=3',
-                        'scikit-image'],
-      python_requires='>=3.5',
-      packages=['pfsspy'],
-      )
+setup(version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass())
---
+++ test.py
@@ -0,0 +1,5 @@
+import sunpy.map
+import pfsspy
+
+m = sunpy.map.Map('/Users/dstansby/sunpy/data/hmi.mrsynop_small_720s.2210.synopMr.fits')
+print(m)
