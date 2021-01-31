9192c1866025d6c05a52f7ef5b23761afe942347
David Stansby <dstansby@gmail.com>
2021-01-31 17:17:16 +0000

    Fix coord test

--- pfsspy/coords.py
+++ pfsspy/coords.py
@@ -10,16 +10,18 @@ import numpy as np
 __all__ = ['strum2cart', 'cart2strum']
 
 
-def strum2cart(rho, s, phi):
+@u.quantity_input
+def strum2cart(rho, s, phi: u.rad):
     """
     Convert strumfric coordinates to cartesian coordinates.
     """
     r = np.exp(rho) * const.R_sun
-    theta = np.arccos(s) * u.rad
+    theta = np.arccos(s)
     return spherical_to_cartesian(r, theta, phi)
 
 
-def cart2strum(x, y, z):
+@u.quantity_input
+def cart2strum(x: u.m, y: u.m, z: u.m):
     """
     Convert cartesian coordinates to strumfric coordinates.
 
--- pfsspy/tests/test_coords.py
+++ pfsspy/tests/test_coords.py
@@ -1,10 +1,14 @@
-import pfsspy.coords
+import astropy.units as u
 import numpy as np
+
+import pfsspy.coords
 
 
 def test_forward_backwards():
-    x, y, z = 1, 0, 0
-    rho, s, phi = pfsspy.coords.cart2strum(x, y, z)
+    xyz = [1, 0, 0] * u.m
+    rho, s, phi = pfsspy.coords.cart2strum(*xyz)
     x1, y1, z1 = pfsspy.coords.strum2cart(rho, s, phi)
 
-    assert np.allclose(np.array([x, y, z]), np.array([x1, y1, z1]))
+    assert u.allclose(xyz[0], x1)
+    assert u.allclose(xyz[1], y1)
+    assert u.allclose(xyz[2], z1)
