f04f7b74510f46534297d1ccccec49e659a8801c
David Stansby <dstansby@gmail.com>
2021-05-21 16:31:32 +0100

    Fix step sizes in fortran tracer

--- pfsspy/tests/test_tracers.py
+++ pfsspy/tests/test_tracers.py
@@ -11,7 +11,8 @@ from .example_maps import dipole_map, dipole_result
 
 
 @pytest.fixture(params=[tracing.PythonTracer(),
-                        tracing.FortranTracer()])
+                        tracing.FortranTracer()],
+                ids=['python', 'fortran'])
 def flines(dipole_result, request):
     tracer = request.param
     input, out = dipole_result
@@ -37,6 +38,25 @@ def test_field_lines(flines):
 def test_fline_in_bounds(flines):
     assert np.all(flines[0].coords.radius >= const.R_sun)
     assert np.all(flines[0].coords.radius <= 2.5 * const.R_sun)
+
+
+def test_fline_step_size(dipole_result):
+    input, out = dipole_result
+    out_frame = out.coordinate_frame
+    seed = coord.SkyCoord(2*u.deg, -45*u.deg, 1.01*const.R_sun,
+                          frame=out_frame)
+
+    tracer = tracing.FortranTracer(step_size=0.5)
+    flines = tracer.trace(seed, out)
+    assert out.grid.nr == 10
+    # With a step size of 0.5, this should be ~20
+    assert flines[0].coords.shape[0] == 21
+
+    tracer = tracing.FortranTracer(step_size=0.2)
+    flines = tracer.trace(seed, out)
+    assert out.grid.nr == 10
+    # With a step size of 0.2, this should be ~50
+    assert flines[0].coords.shape[0] == 52
 
 
 def test_rot_warning(dipole_result):
